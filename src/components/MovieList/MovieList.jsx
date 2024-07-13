6786a6a463f01c2caf833162a964d65ddce36fac
Sara <saraakbarzadeh2004@gmail.com>
2024-07-13 15:20:40 +0330

    changes on navbar

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MovieList.css";
+
+const MovieList = () => {
+  return <section className="move_list"></section>;
+};
+
+export default MovieList;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Navbar.css";
+import Fire from "../../assets/fire.png";
+import Star from "../../assets/glowing-star.png";
+import Party from "../../assets/partying-face.png";
 
 const Navbar = () => {
-  return <nav className="navbar">Navbar content</nav>;
+  return (
+    <nav className="navbar">
+      <h1>MovieManiac</h1>
+
+      <div className="navbar_links">
+        <a href="">
+          Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
+        </a>
+        <a href="">
+          Top Rated <img src={Star} alt="star emoji" className="navbar_emoji" />
+        </a>
+        <a href="">
+          Upcoming{" "}
+          <img src={Party} alt="party emoji" className="navbar_emoji" />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
