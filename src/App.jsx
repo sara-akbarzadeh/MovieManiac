79ceabbf744c06ab68b4cfec18c767496d9230ca
Sara <saraakbarzadeh2004@gmail.com>
2024-07-13 23:58:46 +0330

    added movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/MovieList/MovieList";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <main>Main content</main>
+      <MovieList />
     </div>
   );
 };
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import "./MovieList.css";
+import Fire from "../../assets/fire.png";
+import MovieCard from "./MovieCard";
 
 const MovieList = () => {
-  return <section className="move_list"></section>;
+  return (
+    <section className="move_list">
+      <header className="align_center movie_list_header">
+        <h2 className="align_center movie_list_heading">
+          Popular <img src={Fire} alt="fire emoji" className="navber_emoji" />
+        </h2>
+
+        <div className="align_center movie_list_fs">
+          <ul className="align_center movie_filter">
+            <li className="movie_filter_item active">8+ Star</li>
+            <li className="movie_filter_item">7+ Star</li>
+            <li className="movie_filter_item">6+ Star</li>
+          </ul>
+
+          <select name="" id="" className="movie_sorting">
+            <option value="">SortBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
+          </select>
+
+          <select name="" id="" className="movie_sorting">
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
+          </select>
+        </div>
+      </header>
+      <div className="movie_cards">
+        <MovieCard />
+      </div>
+    </section>
+  );
 };
 
 export default MovieList;
